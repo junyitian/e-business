91d790cb879a312c8d0ff5c9bc2831215bf71997
junyitian <marxpane@gmail.com>
2015-08-19 16:55:37 +0800

    init

---
+++ src/main/java/com/oppx/Application.java
@@ -0,0 +1,14 @@
+package com.oppx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by tianjunyi on 15-8-19.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
